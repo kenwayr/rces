1bdbb6d64ab5ceeaced3382109429bae481a9763
Sagnik Modak <sagnikmodak1118@gmail.com>
2019-08-30 14:31:48 +0530

    new app name + features

--- public/admin/js/qr.generator.js
+++ public/admin/js/qr.generator.js
@@ -1,13 +1,29 @@
 class QRGenerator {
     constructor() {
         this.root = document.createElement('div');
+
+        var wrapperHTML = `
+            <div class="header">
+                <i class="icon fire-mono"></i>
+                FIRE<br/>
+                (Formative Interaction and Rapid Evaluation)</p>
+            </div>
+            <div class="body">
+                
+            </div>
+            <div class="footer">
+                Take care of this position marker. It triggers your attendance.
+            </div>`;
+        var el = document.createElement('div');
+        el.innerHTML = wrapperHTML;
+        el.className = 'card';
         this.template = {
             container: document.createElement('div'),
             image: document.createElement('div'),
-            label: document.createElement('span')
+            label: document.createElement('span'),
+            wrapper: el
         };
-        this.template.container.style = 'display: inline-block; margin: 20px;';
-        this.template.image.style = 'display: block;margin:25px;';
+        this.template.image.style = 'display: block; margin: 0.2cm auto; width: 100%; padding-top: 0.5cm;';
         this.template.label.style = 'display: block; text-align: center; font-size: 12pt; font-weight: bold; color: #000;';
         this.codes = [];
     }
@@ -22,22 +38,38 @@ class QRGenerator {
             var image = this.template.image.cloneNode();
             var qrcode = new QRCode(image, {
                 text: this.codes[i].code,
-                width: 256,
-                height: 256,
+                width: 128,
+                height: 128,
                 colorDark : "#000000",
                 colorLight : "#ffffff",
                 correctLevel : QRCode.CorrectLevel.H
             });
             
             var label = this.template.label.cloneNode();
+            var wrapper = this.template.wrapper.cloneNode(true);
             label.textContent = this.codes[i].label;
             container.appendChild(image);
             container.appendChild(label);
-            this.root.appendChild(container);
+            wrapper.getElementsByClassName('body')[0].appendChild(container);
+            this.root.appendChild(wrapper);
         }
 
         var PrintWindow = window.open('','');
+
+        var iconsheet = document.createElement('link');
+        iconsheet.rel = 'stylesheet';
+        iconsheet.type = 'text/css';
+        iconsheet.href = window.location.origin + '/common/css/icons.css';
+        iconsheet.media = 'all';
+
+        var stylesheet = document.createElement('link');
+        stylesheet.rel = 'stylesheet';
+        stylesheet.type = 'text/css';
+        stylesheet.href = window.location.origin + '/admin/template/default.css';
+        stylesheet.media = 'all';
+
+        PrintWindow.document.head.appendChild(iconsheet);
+        PrintWindow.document.head.appendChild(stylesheet);
         PrintWindow.document.body.appendChild(this.root);
-        PrintWindow.print();
     }
 }
